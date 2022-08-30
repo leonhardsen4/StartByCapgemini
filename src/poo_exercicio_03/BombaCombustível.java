dc82d5c62c737c45443dd463be8d9e734aa090cb
leonhardsen4 <leonhardsen4@gmail.com>
2022-08-30 11:52:52 -0300

    atualização do exercício 3 de POO

--- src/poo_exercicio_03/BombaCombustível.java
+++ src/poo_exercicio_03/BombaCombustível.java
@@ -6,4 +6,34 @@ public class BombaCombustível {
 	private float valorLitro;
 	private float qtdCombustivel;
 	
+	public BombaCombustível(String tipoCombustivel, float valorLitro, float qtdCombustivel) {
+		this.tipoCombustivel = tipoCombustivel;
+		this.valorLitro = valorLitro;
+		this.qtdCombustivel = qtdCombustivel;
+	}
+
+	public String getTipoCombustivel() {
+		return tipoCombustivel;
+	}
+
+	public void setTipoCombustivel(String tipoCombustivel) {
+		this.tipoCombustivel = tipoCombustivel;
+	}
+
+	public float getValorLitro() {
+		return valorLitro;
+	}
+
+	public void setValorLitro(float valorLitro) {
+		this.valorLitro = valorLitro;
+	}
+
+	public float getQtdCombustivel() {
+		return qtdCombustivel;
+	}
+
+	public void setQtdCombustivel(float qtdCombustivel) {
+		this.qtdCombustivel = qtdCombustivel;
+	}
+	
 }
